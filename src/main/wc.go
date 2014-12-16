0d279ad3f0b60114b5129918f7cdc5a928df5302
Lokesh Kumar <lokeshk@Lokeshs-Mac-mini.local>
2014-12-16 09:14:45 -0800

    Lab 1-1

--- src/main/wc.go
+++ src/main/wc.go
@@ -4,15 +4,45 @@ import "os"
 import "fmt"
 import "mapreduce"
 import "container/list"
+import "strconv"
+import "strings"
+import "unicode"
 
 // our simplified version of MapReduce does not supply a
 // key to the Map function, as in the paper; only a value,
 // which is a part of the input file contents
 func Map(value string) *list.List {
+	var l = list.New()
+
+	isNotLetter := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+
+	var words = strings.FieldsFunc(value, isNotLetter)
+	for index := range words {
+		var word = words[index]
+		if len(word) == 0 {
+			continue
+		}
+		//fmt.Println(word)
+		item := mapreduce.KeyValue{word, "1"}
+		l.PushBack(item)
+	}
+	return l
 }
 
 // iterate over list and add values
 func Reduce(key string, values *list.List) string {
+	var sum = 0
+	for e := values.Front(); e != nil; e = e.Next() {
+		var value = e.Value.(string)
+		var count, err = strconv.Atoi(value)
+		if err != nil {
+			panic(err)
+		}
+		sum += count
+	}
+	return strconv.Itoa(sum)
 }
 
 // Can be run in 3 ways:
@@ -20,17 +50,17 @@ func Reduce(key string, values *list.List) string {
 // 2) Master (e.g., go run wc.go master x.txt localhost:7777)
 // 3) Worker (e.g., go run wc.go worker localhost:7777 localhost:7778 &)
 func main() {
-  if len(os.Args) != 4 {
-    fmt.Printf("%s: see usage comments in file\n", os.Args[0])
-  } else if os.Args[1] == "master" {
-    if os.Args[3] == "sequential" {
-      mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
-    } else {
-      mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])    
-      // Wait until MR is done
-      <- mr.DoneChannel
-    }
-  } else {
-    mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
-  }
+	if len(os.Args) != 4 {
+		fmt.Printf("%s: see usage comments in file\n", os.Args[0])
+	} else if os.Args[1] == "master" {
+		if os.Args[3] == "sequential" {
+			mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
+		} else {
+			mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])
+			// Wait until MR is done
+			<-mr.DoneChannel
+		}
+	} else {
+		mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
+	}
 }
